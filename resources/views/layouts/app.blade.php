779b9768d2e6247f6eed85cc5571cac1ea922437
Guilherme Capanema <guilherme@capanema.me>
2020-11-30 08:01:41 -0300

    Commit de mudanças não comitadas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,12 +40,13 @@
 	<script type="text/javascript">
 		var _paq = _paq || [];
 		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(["setCookieDomain", "*.planbicibh.org"]);
 		_paq.push(['trackPageView']);
 		_paq.push(['enableLinkTracking']);
 		(function() {
-			var u="//stats.mobilidadebh.org/";
+			var u="//analytics.horizontes.info/";
 			_paq.push(['setTrackerUrl', u+'piwik.php']);
-			_paq.push(['setSiteId', '2']);
+			_paq.push(['setSiteId', '5']);
 			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 		})();
